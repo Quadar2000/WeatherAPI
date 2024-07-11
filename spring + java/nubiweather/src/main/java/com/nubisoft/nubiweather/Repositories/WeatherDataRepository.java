ca323bd7253802ae1372d51a8b3f4cc6c43366e0
Emil Styrcz <as777@op.pl>
2024-07-11 11:59:06 +0200

    v2 fixes

--- spring + java/nubiweather/src/main/java/com/nubisoft/nubiweather/Repositories/WeatherDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nubisoft.nubiweather.Repositories;
-
-import com.nubisoft.nubiweather.models.WeatherData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WeatherDataRepository extends MongoRepository<WeatherData, String> {
-}
